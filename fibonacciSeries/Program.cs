48fba0e1e48e9b3a1184d6d56dd6b50a9b1fa7c0
iamkanchan01 <83681869+iamkanchan01@users.noreply.github.com>
2022-07-19 16:10:26 +0530

    Add files via upload

---
+++ fibonacciSeries/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fibonacciSeries
+{
+    class Program
+    {
+        static int p1 = 0, p2 = 1, p3;
+        static void Main(string[] args)
+        {
+            //using iterative method
+            Console.WriteLine("Enter input value : ");
+            int count = Convert.ToInt32( Console.ReadLine());
+            Console.Write(p1 + " " + p2);
+            for(int i=2; i<count; i++)
+            {
+                p3 = p1 + p2;
+                Console.Write( " " + p3);
+                p1 = p2;
+                p2 = p3;
+
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ fibonacciWithRecursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fibonacciWithRecursion
+{
+    class Program
+    {
+        static int p1 = 0, p2 = 1, p3;
+        static void Main(string[] args)
+        {
+            //using recursion  method
+            Console.Write("Enter input value : ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.Write(p1 + " " + p2);
+            Fiboacci(count - 2);
+        }
+
+        private static void Fiboacci(int count)
+        {
+            if (count > 0)
+            {
+                p3 = p1 + p2;
+                Console.Write(" " + p3);
+                p1 = p2;
+                p2 = p3;
+                Fiboacci(count - 1);
+
+            }
+            
+        }
+    }
+}
