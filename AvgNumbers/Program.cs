091a4f29e95304c03adae2435066374d50fcb2d3
kanchan-99 <83681869+kanchan-99@users.noreply.github.com>
2022-07-19 14:51:33 +0530

    Add files via upload

---
+++ AvgNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AvgNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c, d;
+            double avg;
+
+            Console.Write("Enter 1st no : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd no : ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 3rd no : ");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 4th no : ");
+            d = Convert.ToInt32(Console.ReadLine());
+
+            avg = (a + b + c + d) / 4;
+            Console.WriteLine("Average is : " + avg);
+
+
+        }
+    }
+}
