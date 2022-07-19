a79d4173ec1761bfd05a5af26ae37c4a832a663f
iamkanchan01 <83681869+iamkanchan01@users.noreply.github.com>
2022-07-19 15:16:06 +0530

    Add files via upload

---
+++ factorialNo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace factorialNo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Factorial Number :");
+            int factNo= Int32.Parse( Console.ReadLine());
+
+            int factResult = 1;
+            for(int i=1; i<factNo; i++)
+            {
+                factResult = factResult * i;
+            }
+            Console.WriteLine("{0}! is {1}", factNo, factResult);
+
+        }
+    }
+}
---
+++ factorialUsingRecursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace factorialUsingRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Fcatorial No : ");
+            int factNo =Int32.Parse(Console.ReadLine());
+            int result = FactorialCalc(factNo);
+            Console.WriteLine(" Result is : {0}", result);
+
+
+        }
+        static int fact = 1;
+        private static int FactorialCalc(int factNo)
+        {
+            
+            if (factNo > 0)
+            {
+                fact = factNo * FactorialCalc(factNo - 1);
+            }
+            return fact;
+        }
+    }
+}
